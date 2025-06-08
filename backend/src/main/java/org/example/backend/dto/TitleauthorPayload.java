0ef7b51c03ca94c9ccd5636a8b17a580e3f6d077
dar0408 <dsalgotra2021@gmail.com>
2025-06-08 10:30:37 +0530

    [Darpan] refactor : Endpoints of TitleAuthorController

--- backend/src/main/java/org/example/backend/dto/TitleauthorPayload.java
+++ backend/src/main/java/org/example/backend/dto/TitleauthorPayload.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TitleauthorIdDto implements Serializable {
-    private String auId;
-    private String titleId;
+public class TitleauthorPayload {
+    private TitleauthorIdDto idDto;
+    private TitleauthorDto dto;
 }
