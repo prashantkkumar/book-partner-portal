f6a6251d17df154fb77a002024fb1eac10ca11e9
PrashantKumar_2110991043 <prashant1043.be21@chitkara.edu.in>
2025-06-09 19:06:42 +0530

    Prashant Testing Jenkins pipeline

--- backend/src/main/java/org/example/backend/controllers/TitleController.java
+++ backend/src/main/java/org/example/backend/controllers/TitleController.java
@@ -117,4 +117,9 @@ public class TitleController {
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
+
+    @GetMapping("/test")
+    public String testEndpoint() {
+        return "Hello, this is a test response!";
+    }
 }
